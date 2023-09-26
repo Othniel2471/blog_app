16300ba66c355d80aa943cbcd9715bebdd984874
othniel2471 <othnielomaidu@gmail.com>
2023-09-26 11:56:48 +0100

    wrote tests for controllers

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
+    before(:each) do
+      get '/users/3/posts'
+    end
+
     it 'returns http success' do
-      get '/posts/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'if correct template rendered' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'response body includes correct placeholder' do
+      expect(response.body).to include("Here's a List of posts for a given user!!")
     end
   end
 
   describe 'GET /show' do
-    it 'returns http success' do
-      get '/posts/show'
+    before(:each) do
+      get '/users/3/posts/1'
+    end
+
+    it 'response status correct' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'if correct template rendered' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'response body includes correct placeholder' do
+      expect(response.body).to include("Here's post details for users..")
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,16 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
+    before(:each) do
+      get '/users'
+    end
+
     it 'returns http success' do
-      get '/users/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'if correct template rendered' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'response body includes correct placeholder' do
+      expect(response.body).to include("Here's a list of users")
     end
   end
 
   describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
+    before(:each) do
+      get '/users/1'
+    end
+
+    it 'response status correct' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'if correct template rendered' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'response body includes correct placeholder' do
+      expect(response.body).to include("Here's the details for a user")
     end
   end
 end
