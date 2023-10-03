4592a3886f31db5fd2ce45053d67ae474fa74563
othniel2471 <othnielomaidu@gmail.com>
2023-10-03 11:48:04 +0100

    fixed linters again

--- spec/features/posts/show_spec.rb
+++ spec/features/posts/show_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Posts index', type: :feature do
   before :each do
     @user = User.create(name: 'otsama', photo: 'photo', bio: 'software developer', posts_counter: 5)
-    post4 = Post.create(title: 'test title 4', text: 'test text', comment_counter: 1, likes_counter: 1,
+    @post4 = Post.create(title: 'test title 4', text: 'test text', comment_counter: 1, likes_counter: 1,
                         author_id: @user.id)
-    post5 = Post.create(title: 'test title 5', text: 'test text', comment_counter: 1, likes_counter: 1,
+    @post5 = Post.create(title: 'test title 5', text: 'test text', comment_counter: 1, likes_counter: 1,
                         author_id: @user.id)
-    post6 = Post.create(title: 'test title 6', text: 'test text', comment_counter: 1, likes_counter: 1,
+    @post6 = Post.create(title: 'test title 6', text: 'test text', comment_counter: 1, likes_counter: 1,
                         author_id: @user.id)
-    comment = Comment.create(text: 'test text', author_id: @user.id, post_id: post4.id)
-    comment2 = Comment.create(text: 'test text', author_id: @user.id, post_id: post5.id)
-    comment3 = Comment.create(text: 'test text', author_id: @user.id, post_id: post6.id)
-    like = Like.create(author_id: @user.id, post_id: post4.id)
+    @comment = Comment.create(text: 'test text', author_id: @user.id, post_id: @post4.id)
+    @comment2 = Comment.create(text: 'test text', author_id: @user.id, post_id: @post5.id)
+    @comment3 = Comment.create(text: 'test text', author_id: @user.id, post_id: @post6.id)
+    @like = Like.create(author_id: @user.id, post_id: post4.id)
     visit user_posts_path(@user.id)
   end
 
